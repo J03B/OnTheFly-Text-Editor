9d23b61c09abcd3b089f4c25513e4f726ad58b53
J03B <byucrazyfan@gmail.com>
2023-04-10 23:14:40 -0500

    Added API calls for GET and PUT requests.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Revision History (J03B): Added logic to a method that accepts some content and adds it to the database
+export const putDb = async (id, content) => {
+  console.log('PUT from the Database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ id: 1, value : content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Revision History (J03B): Added logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the Database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll(); // Could change to get(1) if needing only one.
+  const result = await request;
+  console.log('result.value', result);
+  return result.value;
+};
 
 initdb();
